314021802d99e33d779073304e9ecf19e14c855f
Rodrigo Soares da Silva <guigocore@gmail.com>
2025-07-14 22:04:41 -0300

    feat: criar componente de Tabela para exibir pedidos de viagem

---
+++ src/resources/js/components/Table.vue
@@ -0,0 +1,37 @@
+<template>
+    <table class="table table-striped table-hover">
+        <thead>
+            <tr>
+                <th v-for="title, key in titles" :key="key" scope="col">{{ title.title }}</th>
+                <th v-if="view.visible || update.visible || remove.visible"></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(obj, index) in data" :key="index">
+                <td v-for="(value, key) in titles" :key="value.title + '-' + key">
+                    <span v-if="value.type == 'text'">
+                        {{ value.cast ? value.cast[obj[key]] ?? obj[key] : obj[key] }}
+                    </span>
+                    <span v-if="value.type == 'date'">
+                        {{ formatDateTime(obj[key]) }}
+                    </span>
+                </td>
+                <td v-if="view.visible || update.visible || remove.visible">
+                    <button v-if="view.visible" class="btn btn-outline-success btn-sm" :data-bs-toggle="view.dataToggle" :data-bs-target="view.dataTarget" @click="setStore(obj)">Ver</button>
+                    <button v-if="update.visible" class="btn btn-outline-primary btn-sm" :data-bs-toggle="update.dataToggle" :data-bs-target="update.dataTarget" @click="setStore(obj)">Editar</button>
+                    <button v-if="remove.visible" class="btn btn-outline-danger btn-sm" :data-bs-toggle="remove.dataToggle" :data-bs-target="remove.dataTarget" @click="setStore(obj)">Remover</button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script setup>
+    import { formatDateTime } from '@/utils/date';
+</script>
+
+<script>
+    export default {
+        props: ['data', 'titles', 'view', 'update', 'remove', ],
+    }
+</script>
